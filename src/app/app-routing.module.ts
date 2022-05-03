1247f400d62f5f461802640fa65347b38d754e32
manjunathhalli <manjunathhalli629@gmail.com>
2022-05-03 14:02:33 +0530

    Added : Login page form and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './Components/registration/registration.component';
+import { LoginComponent } from './Components/login/login.component';
 
-const routes: Routes = [{path:'register',component:RegistrationComponent}];
+
+
+const routes: Routes = [{path:'register',component:RegistrationComponent},
+{path:'login',component:LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
