ca7c0e368fc0e8391df7256dea98f406a6f5d2c9
manjunathhalli <manjunathhalli629@gmail.com>
2022-05-05 07:59:01 +0530

    Added : validation for forgot,reset page

--- src/app/Components/forgot/forgot.component.ts
+++ src/app/Components/forgot/forgot.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-forgot',
   templateUrl: './forgot.component.html',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotComponent implements OnInit {
 
+  ForgotForm! : FormGroup;
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.ForgotForm = new FormGroup({
+      email: new FormControl('',[Validators.required,Validators.email])
+    })
 
-}
+  }
+  getEmail(){
+    return this.ForgotForm.get('email')?.value;
+  }
+  }
+  
--- src/app/Components/reset/reset.component.ts
+++ src/app/Components/reset/reset.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-reset',
   templateUrl: './reset.component.html',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetComponent implements OnInit {
 
+  email = "";
+  hide = false;
+  ResetForm!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.ResetForm = new FormGroup({
+      password: new FormControl('', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*]).{8,}')]),
+      confirm: new FormControl('', Validators.required)
+    })
   }
-
 }
